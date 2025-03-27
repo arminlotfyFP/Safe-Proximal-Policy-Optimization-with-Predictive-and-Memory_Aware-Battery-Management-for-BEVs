146b6a3c5eee0de2a14470b5fd3680b6d7ccafec
Armin <aarmi.lotfi@gmail.com>
2025-03-27 14:38:58 -0400

    nothing

--- paper3_predictor.py
+++ paper3_predictor.py
@@ -8,10 +8,20 @@ import mat73
 import h5py
 import scipy.io as scipy
 print(os.getcwd())
-os.chdir("/home/armin/Desktop/paper3_python")
+os.chdir("/home/armin/Desktop/paper3_python/paper3_py")
 
 
 data = mat73.loadmat('data.mat')
 data = scipy.loadmat('data.mat')
-# data = h5py.File('data.mat')
-# data = scipy.io.loadmat('data.mat')
+
+data = data["data"]
+X = np.delete(data, [6,9,11], axis=1)
+Y = data[:,4]
+
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+
+MM = MinMaxScaler()
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size=0.2, shuffle=False)
+X_train_n = MM.fit_transform(X_train)
+X_test_n = MM.transform(X_test)
